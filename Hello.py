bb9c18342c1e7863a35beceac569bfd7cc117eed
Sho Maeoki <32443681+okisy@users.noreply.github.com>
2023-10-08 19:27:43 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,42 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
 
-LOGGER = get_logger(__name__)
 
+def main():
+    llm = ChatOpenAI(temperature=0)
 
-def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="My Great ChatGPT",
+        page_icon="🤗"
     )
+    st.header("My Great ChatGPT 🤗")
 
-    st.write("# Welcome to Streamlit! 👋")
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content="You are a helpful assistant.")
+        ]
 
-    st.sidebar.success("Select a demo above.")
+    # Monitor user input
+    if user_input := st.chat_input("Input your question here:"):
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        with st.spinner("ChatGPT is typing ..."):
+            response = llm(st.session_state.messages)
+        st.session_state.messages.append(AIMessage(content=response.content))
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    # Display chat history
+    messages = st.session_state.get('messages', [])
+    for message in messages:
+        if isinstance(message, AIMessage):
+            with st.chat_message('assistant'):
+                st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message('user'):
+                st.markdown(message.content)
+        else:  # isinstance(message, SystemMessage):
+            st.write(f"System message: {message.content}")
 
 
-if __name__ == "__main__":
-    run()
+if __name__ == '__main__':
+    main()
